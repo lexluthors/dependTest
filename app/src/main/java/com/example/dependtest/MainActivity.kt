85762843eeeecaacb75bee3772a55b098ebed430
liujie <594134383@qq.com>
2021-07-26 15:28:23 +0800

    测试3.0

--- app/src/main/java/com/example/dependtest/MainActivity.kt
+++ app/src/main/java/com/example/dependtest/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.dependtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.szkingdom.dialog.view.CustomDialog
+import com.example.gytestlib.Depend
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var dialog = CustomDialog(this)
+        Depend.getTest()
     }
 }
---
+++ gyTestLib/src/main/java/com/example/gytestlib/Depend.java
@@ -0,0 +1,12 @@
+package com.example.gytestlib;
+
+/**
+ * Description:
+ * Data：2021/7/26-下午3:25
+ * Author: Allen
+ */
+public class Depend {
+    public static void getTest(){
+
+    }
+}
