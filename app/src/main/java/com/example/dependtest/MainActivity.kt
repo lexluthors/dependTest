bc86b0995d1efff4fe02e40a0ae7ba8e63c6e609
liujie <594134383@qq.com>
2021-07-26 15:17:55 +0800

    测试2.0

--- app/src/main/java/com/example/dependtest/MainActivity.kt
+++ app/src/main/java/com/example/dependtest/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.dependtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.szkingdom.dialog.view.CustomDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var dialog = CustomDialog(this)
     }
 }
